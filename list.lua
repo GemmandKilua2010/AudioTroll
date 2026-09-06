edb6d656b9243287fb5903a724ca11aa19222113
GemmandKilua2010 <specterxofc@outlook.com>
2026-09-05 23:54:54 -0300

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,44 @@
+local LIST = {
+    {
+        name = "ahhhhhhhaura",
+        id = "99046832283034"
+    },
+    {
+        name = "aura",
+        id = "107705845357163"
+    },
+    {
+        name = "raio",
+        id = "97591643459481"
+    },
+    {
+        name = "galinha",
+        id = "93716838610267"
+    },
+    {
+        name = "ventilador",
+        id = "82734295073558"
+    },
+    {
+        name = "Sukuna",
+        id = "139666537620285"
+    },
+    {
+        name = "Windows",
+        id = "125936118513893"
+    },
+    {
+        name = "Windows erro",
+        id = "2661731024"
+    },
+    {
+        name = "Ahhhhh",
+        id = "138326175698800"
+    },
+    {
+        name = "Faz o L",
+        id = "133157583643691"
+    }
+}
+
+return LIST
